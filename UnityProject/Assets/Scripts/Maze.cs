5ed008f754526796a6bc7b642b07fc83e09a4bcb
Anton Roy <antonmakesgames@gmail.com>
2020-09-20 21:58:03 +0200

    Commiting changes I forgot to archive.

---
+++ UnityProject/Assets/Scripts/Maze.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AMG
+{
+    public class Maze : MonoBehaviour
+    {
+        public int Size = 5;
+        private VectorPair[] Walls;
+    }
+}
---
+++ UnityProject/Assets/Scripts/VectorPair.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AMG
+{
+    public struct VectorPair
+    {
+        public Vector2 V1;
+        public Vector2 V2;
+        public Vector2 Normale;
+
+        public float Magnitude()
+        {
+            return (this.V1 - this.V2).magnitude;
+        }
+
+        public void ComputeNormale()
+        {
+            this.Normale = (this.V2 - this.V1).normalized;
+            float temp = this.Normale.x;
+            this.Normale.x = this.Normale.y;
+            this.Normale.y = temp;
+        }
+    }
+}
